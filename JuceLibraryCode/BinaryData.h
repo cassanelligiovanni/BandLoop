272ff4cd7ad21126c561a4d10bc012854efd64f6
Giovanni <cassanelligiovannia@gmail.com>
2019-05-16 21:51:08 +0100

    Dissertation

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,93 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   Doxyfile;
+    const int            DoxyfileSize = 79645;
+
+    extern const char*   LICENSE_md;
+    const int            LICENSE_mdSize = 1726;
+
+    extern const char*   README_md;
+    const int            README_mdSize = 3574;
+
+    extern const char*   StartPlayingButton_svg;
+    const int            StartPlayingButton_svgSize = 1932;
+
+    extern const char*   StopPlayingButton_svg;
+    const int            StopPlayingButton_svgSize = 501;
+
+    extern const char*   StartRecordingButton_svg;
+    const int            StartRecordingButton_svgSize = 1634;
+
+    extern const char*   StopRecordingButton_svg;
+    const int            StopRecordingButton_svgSize = 1848;
+
+    extern const char*   BarPlayingOff_svg;
+    const int            BarPlayingOff_svgSize = 6046;
+
+    extern const char*   BarPlayingOn_svg;
+    const int            BarPlayingOn_svgSize = 9557;
+
+    extern const char*   BarRecordingOff_svg;
+    const int            BarRecordingOff_svgSize = 6100;
+
+    extern const char*   BarRecordingOn_svg;
+    const int            BarRecordingOn_svgSize = 13325;
+
+    extern const char*   pedal_svg;
+    const int            pedal_svgSize = 2304;
+
+    extern const char*   one_wav;
+    const int            one_wavSize = 26552;
+
+    extern const char*   two_wav;
+    const int            two_wavSize = 17360;
+
+    extern const char*   Fader_png;
+    const int            Fader_pngSize = 11346;
+
+    extern const char*   DeleteTrack_svg;
+    const int            DeleteTrack_svgSize = 1496;
+
+    extern const char*   BPMCounterOFF_svg;
+    const int            BPMCounterOFF_svgSize = 3533;
+
+    extern const char*   BPMDisplay_svg;
+    const int            BPMDisplay_svgSize = 358;
+
+    extern const char*   BPMCounterON_svg;
+    const int            BPMCounterON_svgSize = 5484;
+
+    extern const char*   PlayButton_svg;
+    const int            PlayButton_svgSize = 1575;
+
+    extern const char*   StopButton_svg;
+    const int            StopButton_svgSize = 1347;
+
+    extern const char*   FallingSky_otf;
+    const int            FallingSky_otfSize = 125092;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 22;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
